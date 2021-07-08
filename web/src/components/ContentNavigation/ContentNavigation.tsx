637da796b8f91e910d50089d81ffcd31112b5d05
Morgan Spencer <morgan@spencercreative.co>
2021-07-08 07:14:14 -0400

    Update content nav to handle multiple

--- web/src/components/ContentNavigation/ContentNavigation.tsx
+++ web/src/components/ContentNavigation/ContentNavigation.tsx
@@ -5,40 +5,59 @@ const ContentNavigation = ({ navItems, query }) => {
 
   const params = new URLSearchParams(search)
   const term = params.get(query)
-  const queryString = '?' + query + '='
 
   const firstItem = navItems[0]
 
+  function setUrlParams(value?) {
+    if (!value) {
+      params.delete(query)
+    } else {
+      params.set(query, value)
+    }
+
+    const string = params.toString()
+
+    let path = pathname
+
+    if (string) path += '?' + string
+
+    return path
+  }
+
   return (
-    <nav className="flex items-center flex-wrap gap-2">
-      <Link
-        to={pathname}
-        className={`${
-          term === null
-            ? 'bg-gray-300 hover:bg-gray-200'
-            : 'bg-gray-200 hover:bg-gray-300'
-        } rounded-full flex items-center gap-1 px-3 py-1 text-sm font-semibold hover:text-gray-900 focus:ring-gray-500`}
-      >
-        <firstItem.icon />
-        <span>{firstItem.name}</span>
-      </Link>
-      {navItems.slice(1).map((item, i: number) => (
+    <>
+      <nav className="flex items-center flex-wrap gap-2">
         <Link
-          key={i}
-          to={`${pathname}${item.to ? queryString + item.to : ''}`}
+          to={setUrlParams()}
           className={`${
-            term === item.to
-              ? `bg-${item.color}-200 text-${item.color}-800`
-              : 'bg-gray-200'
-          } rounded-full flex items-center gap-1 px-3 py-1 text-sm font-semibold hover:bg-${
-            item.color
-          }-200 hover:text-${item.color}-800 focus:ring-${item.color}-500`}
+            term === null
+              ? 'bg-gray-300 hover:bg-gray-200'
+              : 'bg-gray-200 hover:bg-gray-300'
+          } rounded-full flex items-center gap-1 px-3 py-1 font-semibold hover:text-gray-900 focus:ring-gray-500 text-xs md:text-sm`}
         >
-          {item.icon && <item.icon />}
-          <span>{item.name}</span>
+          <firstItem.icon />
+          <span>{firstItem.name}</span>
         </Link>
-      ))}
-    </nav>
+        {navItems.slice(1).map((item, i: number) => (
+          <Link
+            key={i}
+            to={setUrlParams(item.to)}
+            className={`${
+              term === item.to
+                ? `bg-${item.color}-200 text-${item.color}-800`
+                : 'bg-gray-200'
+            } rounded-full flex items-center gap-1 px-3 py-1 font-semibold hover:bg-${
+              item.color
+            }-200 text-xs md:text-sm hover:text-${item.color}-800 focus:ring-${
+              item.color
+            }-500`}
+          >
+            {item.icon && <item.icon />}
+            <span>{item.name}</span>
+          </Link>
+        ))}
+      </nav>
+    </>
   )
 }
 
