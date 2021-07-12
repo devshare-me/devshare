60c1626e1d5422cc47784752ec023420acad88ba
Morgan Spencer <morgan@spencercreative.co>
2021-07-11 22:13:54 -0400

    Add custom fonts

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,7 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ['"Source Sans Pro"', ...defaultTheme.fontFamily.sans],
+      mono: ['"Source Code Pro"', ...defaultTheme.fontFamily.mono],
+    },
     extend: {},
   },
   variants: {
