1a4917def8de274b1bbe8101751ebfa84185b958
Morgan Spencer <morgan@spencercreative.co>
2021-07-12 20:54:20 -0400

    Add preview to post form

--- web/src/components/Post/PostForm/PostForm.tsx
+++ web/src/components/Post/PostForm/PostForm.tsx
@@ -10,14 +10,44 @@ import {
   Submit,
 } from '@redwoodjs/forms'
 import { filters } from 'src/utils/filters'
+import ReactPlayer from 'react-player'
 import { FiCornerUpRight } from 'react-icons/fi'
 
 const PostForm = (props) => {
   const type = props.type ? props.type : props.post.type
+  const [linkUrl, setLinkUrl] = React.useState('')
+  const [imageUrl, setImageUrl] = React.useState('')
+  const [videoUrl, setVideoUrl] = React.useState('')
+  const [urlWorks, setUrlWorks] = React.useState(true)
+
+  const isValidUrl = (url: string) => {
+    const regexp =
+      /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/
+
+    if (regexp.test(url)) {
+      return true
+    } else {
+      return false
+    }
+  }
 
   const onSubmit = (data) => {
     data.type = type
     props.onSave(data, props?.post?.id)
+  }
+
+  const setUrl = (e) => {
+    const value = e.target.value
+    setUrlWorks(true)
+
+    if (type === 'video') {
+      setVideoUrl(value)
+      setUrlWorks(ReactPlayer.canPlay(value))
+    } else if (type === 'image') {
+      setImageUrl(value)
+    } else {
+      setLinkUrl(value)
+    }
   }
 
   let filter = filters.find(
@@ -72,6 +102,7 @@ const PostForm = (props) => {
               errorClassName="rw-input rw-input-error"
               placeholder={`${type} URL`}
               validation={{ required: true }}
+              onChange={(e) => setUrl(e)}
             />
             <FieldError name="url" className="rw-field-error" />
           </>
@@ -133,6 +164,27 @@ const PostForm = (props) => {
             />
           </>
         )}
+
+        {['link', 'image', 'video'].includes(type) &&
+          (linkUrl || imageUrl || videoUrl) && (
+            <div className="my-2">
+              <p className="font-semibold">Preview:</p>
+              {!urlWorks && (
+                <div className="text-red-700 font-semibold">
+                  URL is not valid or is not compatible. Please check the URL
+                  and try again.
+                </div>
+              )}
+              {type === 'video' &&
+                videoUrl &&
+                isValidUrl(videoUrl) &&
+                urlWorks && (
+                  <div className="aspect-w-16 aspect-h-9">
+                    <ReactPlayer url={videoUrl} width="100%" height="100%" />
+                  </div>
+                )}
+            </div>
+          )}
 
         <div className="flex md:items-center justify-between flex-col md:flex-row mt-6 p-6 -mx-6 -mb-6 bg-gray-50 border-t border-solid border-gray-200">
           <div
