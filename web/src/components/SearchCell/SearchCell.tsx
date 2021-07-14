571dcceff743f880872f4801b264cdd6531132ef
Morgan Spencer <morgan@spencercreative.co>
2021-07-13 21:43:10 -0400

    Update search loader

--- web/src/components/SearchCell/SearchCell.tsx
+++ web/src/components/SearchCell/SearchCell.tsx
@@ -26,12 +26,13 @@ export const Loading = () => (
         <ProfileLoader />
         <ProfileLoader />
         <ProfileLoader />
-        <ProfileLoader />
-        <ProfileLoader />
-        <ProfileLoader />
       </>
     }
-    posts={<PostLoader />}
+    posts={
+      <div className="-mt-4">
+        <PostLoader />
+      </div>
+    }
   />
 )
 
@@ -41,23 +42,28 @@ export const Failure = ({ error }: CellFailureProps) => (
   <div style={{ color: 'red' }}>Error: {error.message}</div>
 )
 
-export const Success = ({ search, query }: CellSuccessProps<FindSearchQuery>) => {
+export const Success = ({
+  search,
+  query,
+}: CellSuccessProps<FindSearchQuery>) => {
   return (
     <SearchLayout
       users={
         <>
-          {search.users.length > 0 ? search.users.map((user) => (
-            <ProfileItem key={user.id} user={user} link={true} />
-          )) : (
+          {search.users.length > 0 ? (
+            search.users.map((user) => (
+              <ProfileItem key={user.id} user={user} link={true} />
+            ))
+          ) : (
             <p>No users found</p>
           )}
         </>
       }
       posts={
         <>
-          {search.posts.length > 0 ? search.posts.map((post) => (
-            <FeedItem key={post.id} item={post} />
-          )) : (
+          {search.posts.length > 0 ? (
+            search.posts.map((post) => <FeedItem key={post.id} item={post} />)
+          ) : (
             <p>No posts found</p>
           )}
         </>
@@ -66,20 +72,16 @@ export const Success = ({ search, query }: CellSuccessProps<FindSearchQuery>) =>
   )
 }
 
-const SearchLayout = ({posts, users}) => {
+const SearchLayout = ({ posts, users }) => {
   return (
     <div className="flex flex-col lg:flex-row lg:items-start">
       <div className="w-full mb-8 lg:max-w-xs lg:mr-8 lg:mb-0">
         <h2 className="font-bold text-xl mb-2">Users</h2>
-        <div className="grid grid-cols-1 gap-4 mt-4">
-          {users}
-        </div>
+        <div className="grid grid-cols-1 gap-4 mt-4">{users}</div>
       </div>
       <div className="flex-1">
         <h2 className="font-bold text-xl mb-2">Posts</h2>
-        <div className="grid grid-cols-1 gap-4 mt-4">
-          {posts}
-        </div>
+        <div className="grid grid-cols-1 gap-4 mt-4">{posts}</div>
       </div>
     </div>
   )
