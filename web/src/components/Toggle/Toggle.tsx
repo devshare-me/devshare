151eef7963a5c1fe5e8971b9a97871eea4a4350e
Morgan Spencer <morgan@spencercreative.co>
2021-07-15 16:35:41 -0400

    Update toggle size and ring color

--- web/src/components/Toggle/Toggle.tsx
+++ web/src/components/Toggle/Toggle.tsx
@@ -17,11 +17,11 @@ const Toggle = ({
           className={`${
             enabled ? `bg-${color}-500` : 'bg-gray-400 dark:bg-gray-600'
           }
-          relative inline-flex flex-shrink-0 h-6 w-12 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+          relative inline-flex flex-shrink-0 h-6 w-10 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2  focus:ring-${color}-300 focus:ring-opacity-75`}
         >
           <span
             aria-hidden="true"
-            className={`${enabled ? 'translate-x-6' : 'translate-x-0'}
+            className={`${enabled ? 'translate-x-4' : 'translate-x-0'}
             pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow-lg transform ring-0 transition ease-in-out duration-200`}
           />
         </Switch>
