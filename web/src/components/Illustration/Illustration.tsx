72ef0ca7447456c5da1c7725c1ca2678c0b49174
Morgan Spencer <morgan@spencercreative.co>
2021-07-19 21:07:55 -0400

    Add custom error page designs

--- web/src/components/Illustration/Illustration.tsx
+++ web/src/components/Illustration/Illustration.tsx
@@ -3,7 +3,7 @@ const Illustration = (props) => {
     <div className="max-w-sm w-full mx-auto text-center py-12">
       <props.image className="w-full h-auto" />
       {props.message && (
-        <p className="font-semibold mt-4 text-gray-500 text-lg">
+        <p className="font-semibold mt-4 text-gray-500 dark:text-gray-400 text-lg">
           {props.message}
         </p>
       )}
